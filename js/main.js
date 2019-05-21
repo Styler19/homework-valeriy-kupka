9504d086f0acea5b29ec58598c783198db849aab
Styler19 <mr.styler2018@gmail.com>
2019-05-21 21:44:38 +0300

    hw 10

--- js/main.js
+++ js/main.js
@@ -1,174 +1,193 @@
-//  1. Используя rest оператор и деструктуризацию, создать функцию, которая принимает любое количество аргументов и возвращает объект, содержащий первый аргумент и массив из остатка:
-//  func(‘a’, ‘b’, ‘c’, ‘d’) → 
-//  {
-//    first: ‘a’,
-//    other: [‘b’, ‘c’, ‘d’]
-//  }
+//  1. Создайте функцию которая бы умела делать:
+//      minus(10)(6); // 4
+//      minus(5)(6); // -1
+//      minus(10)(); // 10
+//      minus()(6); // -6
+//      minus()(); // 0
+//      Подсказка, функция minus должна возвращать другую функцию.
 
-const firstAndOther = ( first, ...other ) => {
-    return {
-        first: first,
-        other: other
+function minus (number1 = 0) {
+    return function(number2 = 0) {
+        return number1 - number2;
     };
 };
 
-console.log('1.', firstAndOther('a', 'b', 'c', 'd'));
+//  2. Реализовать функцию, которая умножает и умеет запоминать возвращаемый результат между вызовами:
+//      function multiplyMaker ...
+//      const multiply = multiplyMaker(2);
+//      multiply(2); // 4 (2 * 2)
+//      multiply(1); // 4 (4 * 1)
+//      multiply(3); // 12 (4 * 3)
+//      multiply(10); // 120 (12 * 10)
 
-//  2. Организовать функцию getInfo, которая принимает объект вида
-//  { name: ...,  info: { employees: [...], partners: [ … ]  } }
-//  и выводит в консоль имя (если имени нет, показывать ‘Unknown’) и первые две компании из массива partners:
-
-//  const organisation = {  
-//    name: 'Google',   
-//    info: { employees: [‘Vlad’, ‘Olga’], partners: ['Microsoft', 'Facebook', 'Xing']   } 
-//  };
-//  getInfo(organisation); → 
-//  Name: Google 
-//  Partners: Microsoft Facebook
-
-const organisation = {  
-    name: 'Google',   
-    info: { employees: ['Vlad', 'Olga'], partners: ['Microsoft', 'Facebook', 'Xing']   } 
+function multiplyMaker (mainNumber) {
+    return function(number = 1) {
+        return mainNumber *= number;
+    };
 };
 
-const getInfo = ( {name = 'Unknown', info: { partners: [comp1, comp2] } } ) => {
-    console.log({
-        Name: name,
-        Partners: `${comp1} ${comp2}`
-    });
+const multiply = multiplyMaker(2);
+
+//  3. Реализовать модуль, который работает со строкой и имеет методы:
+//      a. установить строку
+//          i. если передано пустое значение, то установить пустую строку
+//          ii. если передано число, число привести к строке
+//      b. получить строку
+//      c. получить длину строки
+//      d. получить строку-перевертыш
+//      Пример:
+//          модуль.установитьСтроку(‘abcde’);
+//          модуль.получитьСтроку(); // ‘abcde’
+//          модуль.получитьДлину(); // 5
+
+const stringMethods = (function () {
+    let mainString;
+
+    function setString(string = '') {
+        if (typeof(string) === 'number') {
+            string = String(string);
+        }
+        mainString = string;
+
+        return mainString;
+    };
+
+    function getString() {
+        return mainString;
+    };
+
+    function getLengthOfString() {
+        return mainString.length;
+    };
+
+    function getReverseOfstring() {
+        let reversedString = mainString
+            .split('')
+            .reverse()
+            .join('')
+
+        return reversedString;
+    };
+
+    return {
+        setString,
+        getString,
+        getLengthOfString,
+        getReverseOfstring
+    }
+})();
+
+//  4. Создайте модуль “калькулятор”, который умеет складывать, умножать, вычитать, делить и возводить в степень. Конечное значение округлить до двух знаков после точки (значение должно храниться в обычной переменной, не в this).
+//
+//      модуль.установитьЗначение(10); // значение = 10
+//      модуль.прибавить(5); // значение += 5
+//      модуль.умножить(2); // значение *= 2
+//      модуль.узнатьЗначение(); // вывести в консоль 30 (здесь надо округлить)
+//
+//    Также можно вызывать методы цепочкой:
+//        модуль.установитьЗначение(10).вСтепень(2).узнатьЗначение(); // 100
+
+const calc = (function () {
+    let mainNumber;
+
+    function setNumber(number = 0) {
+        mainNumber = number;
+        mainNumber = mainNumber.toFixed(2);
+
+        return this;
+    };
+
+    function getNumber() {
+        return mainNumber;
+    };
+
+    function plus(number = 0) {
+        mainNumber += number;
+        mainNumber = mainNumber.toFixed(2);
+
+        return this;
+    };
+
+    function minus(number = 0) {
+        mainNumber -= number;
+        mainNumber = mainNumber.toFixed(2);
+
+        return this;
+    };
+
+    function multiply(number = 1) {
+        mainNumber *= number;
+        mainNumber = mainNumber.toFixed(2);
+
+        return this;
+    };
+
+    function devide(number = 1) {
+        mainNumber /= number;
+        mainNumber = mainNumber.toFixed(2);
+
+        return this;
+    };
+
+    function raise(number = 1) {
+        for (let index = 1; index < number; index++) {
+            mainNumber *= mainNumber;
+        };
+        mainNumber = mainNumber.toFixed(2);
+
+        return this;
+    };
+
+    return {
+        setNumber,
+        getNumber,
+        plus,
+        minus,
+        multiply,
+        devide,
+        raise
+    };
+})();
+
+console.log('4.', calc.setNumber(10).raise(2).getNumber());
+
+//  1. Создать конструктор для производства автомобилей. Конструктор должен принимать марку автомобиля и возраст машины. Конструктор должен иметь метод, который возвращает марку, и
+//  второй метод, который возвращает год производства машины (год текущий минус возраст машины, использовать Date для получения текущего года)
+//      var lexus = new Car(‘lexus’, 2);
+//      lexus.получитьМарку(); // “Lexus”
+//      lexus.получитьГодВыпуска(); // 2017 (2019-2);
+//      Марка машины всегда должна возвращаться с большой буквы!
+
+function Car(model, age) {
+    const date = new Date();
+
+    this.model = model[0].toUpperCase() + model.slice(1);
+    this.yearOfCarManufacture = date.getFullYear() - age;
 }
 
-getInfo(organisation);
+const lexus = new Car('lexus', 2);
 
-//  1. Переделать функцию с использованием функции-стрелки (в методе reduce тоже использовать arrow function):
-//  
-//  function sum() {
-//    const params = Array.prototype.slice.call(arguments);
-//  
-//    if (!params.length) return 0;
-//  
-//    return params.reduce(function (prev, next) { return prev + next; });
-//  }
-//  
-//  sum(1, 2, 3, 4); // 10
-//  sum(); // 0
+//  2. Написать конструктор, который умеет элементарно шифровать строки (например, сделать из строки строку-перевертыш, или заменить все символы их цифровым представлением, или любой другой метод). Конструктор при инициализации получает строку и имеет следующие методы:
+//      a. показать оригинальную строку
+//      b. показать зашифрованную строку
+//  Строки не должны быть доступны через this, только с помощью методов.
 
-const sum = (...numbers) => {
-    const params = numbers.slice();
+const EncoderOfString = function(string) {
+    function getString() {
+        return string;
+    };
 
-  if (!params.length) return 0;
+    function crypteString() {
+        return string
+            .split('')
+            .reverse()
+            .join('');
+    };
 
-  return params.reduce( (prev, next) => { return prev + next; });
+    return {
+        getString,
+        crypteString
+    }
 };
 
-//  1. Создать объект, который описывает ширину и высоту
-//  прямоугольника, а также может посчитать площадь фигуры:
-//  const rectangle = {width:..., height:..., getSquare:...};
-
-const rectangle = {
-    width: 100,
-    height: 100,
-    getSquare: function() { return this.width * this.height }
-};
-
-//  2. Создать объект, у которого будет цена товара и его скидка, а также
-//  два метода: для получения цены и для расчета цены с учетом скидки:
-//  const price = {
-//      price: 10,
-//      discount: '15%',
-//  ... };
-//  price.getPrice(); // 10
-//  price.getPriceWithDiscount(); // 8.5
-
-const price = {
-    price: 10,
-    discount: '15%',
-    getPrice: function() { return this.price },
-    getPriceWithDiscount: function() { return this.getPrice() / 100 * (100 - parseFloat(this.discount)) }
-};
-
-//  3. Создать объект, у которого будет поле высота и метод “увеличить
-//  высоту на один”. Метод должен возвращать новую высоту:
-//  object.height = 10;
-//  object.inc(); // придумать свое название для метода
-//  object.height; // 11;
-
-const addToHeight = {
-    height: 10,
-    addOneToHeight: function() { return this.height += 1 }
-};
-
-//  4. Создать объект “вычислитель”, у которого есть числовое свойство
-//  “значение” и методы “удвоить”, “прибавить один”, “отнять один”.
-//  Методы можно вызывать через точку, образуя цепочку методов:
-//  const numerator = {
-//      value: 1,
-//      double: function () {...},
-//      plusOne: function () {...},
-//      minusOne: function () {...},
-//  }
-//  numerator.double().plusOne().plusOne().minusOne();
-//  numerator.value // 3
-
-const numerator = {
-    value: 1,
-    double: function() { this.value *= 2; return this;},
-    plusOne: function() { this.value += 1; return this; },
-    minusOne: function() { this.value -= 1; return this; }
-}
-
-numerator.double().plusOne().plusOne().minusOne();
-
-//  1. Создать объект с розничной ценой и количеством продуктов. Этот
-//  объект должен содержать метод для получения общей стоимости
-//  всех товаров (цена * количество продуктов)
-
-const products = {
-    price: '14$',
-    quantity: 203,
-    totalCost: function() { return parseFloat(this.price) * this.quantity + '$' }
-};
-
-//  2. Создать объект из предыдущей задачи. Создать второй объект,
-//  который описывает количество деталей и цену за одну деталь. Для
-//  второго объекта нужно узнать общую стоимость всех деталей, но
-//  нельзя создавать новые функции и методы. Для этого
-//  “позаимствуйте” метод из предыдущего объекта.
-
-const details = {
-    price: '231$',
-    quantity: 658,
-    totalCost: function() { return products.totalCost.call(this) }
-};
-
-//  3. Даны объект и функция:
-//  let sizes = {width: 5, height: 10},
-//  getSquare = function () {return this.width * this.height};
-//  Не изменяя функцию или объект, получить результат функции
-//  getSquare для объекта sizes
-
-let sizes = {width: 5, height: 10},
-getSquare = function () {return this.width * this.height};
-
-console.log('3.', getSquare.call(sizes) );
-
-//  4.  
-//  let element = {
-//      height: 25,
-//      getHeight: function () {return this.height;}
-//  };
-//  
-//  let getElementHeight = element.getHeight;
-//  getElementHeight(); // undefined
-//  
-//  Измените функцию getElementHeight таким образом, чтобы можно
-//  было вызвать getElementHeight() и получить 25.
-
-let element = {
-    height: 25,
-    getHeight: function () {return this.height;}
-};
-
-let getElementHeight = element.getHeight.call(element);
-
-console.log('4.', getElementHeight);
+const string = new EncoderOfString('Hello, Easycode');
