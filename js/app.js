323f8b9c254aeba0d673481d4fd69dc0b6675186
Styler19 <mr.styler2018@gmail.com>
2019-06-01 01:00:30 +0300

    hw 12

--- js/app.js
+++ js/app.js
@@ -1,197 +1,52 @@
-// // // 
+// это корневой файл, который просто управляет тем куда пойти и что сделать
+// при срабатывании того или иного события
 
+// NewsService, NewsUI, LoaderUI классы оьявлены глобально в файлай, которые подключены выше этого
+// и они нам тут теперь доступны для того, что бы собственно "посылать" и "делать"
 
-// for (let index = 0; index < 3; index++) {
-//     // index
-//     setTimeout(() => {
-//         console.log(index);
-//     }, 1000);
-// }
+// инициализация всех необходимый конструкторов
+const newsService = new NewsService()
+const newsUI = new NewsUI()
+const loaderUI = new LoaderUI('.news-wrap .row')
+const notificationUI = new NotificationUI()
 
-// function User(name, age) {
-//     this.name = name
-//     this.age = age
-//     this.getName = function () {
-//         return this.name
-//     }
+// UI Elements
+const form = document.forms['newsControlForm'];
+const countrySelect = form['country'];
+const categorySelect = form['category'];
+const searchInput = form['search'];
 
-//     this.getAge = function () {
-//         return this.age
-//     }
+// Event listeners -- собираем все addEventListener в обну стопку, чтобы потом не искать их по коду
+countrySelect.addEventListener('change', onSelectChange)
+categorySelect.addEventListener('change', onSelectChange)
+searchInput.addEventListener('change', onSearchChange)
 
-//     this.getFullInfo = function () {
-//         return `User Name - ${this.userName}`
-//     }
-// }
+// Handlers for events -- пишем функции-обработчики, которые нужны для addEventListener
+function onSelectChange() {
+    const country = countrySelect.value;
+    const category = categorySelect.value;
 
-// function OtherUser() {
-//     this.userName = 'Other user'
-// }
+    if (!country || !category) return notificationUI.setNotification('Choose the category and country')
 
-// OtherUser.prototype = new User('Pasha', 18)
-
-// const otherUser = new OtherUser()
-
-// const pasha = new User('Pasha', 18);
-// const alina = new User('Alina', 30)
-
-// console.log('Pasha - ', pasha)
-// console.log('Alina - ', alina)
-
-// // // prototype
-// function Car(settings) {
-//     const {
-//         name,
-//         model,
-//         totalMiles = 0,
-//         totalFuelValue = 10
-//     } = settings
-
-//     this.name = name;
-//     this.model = model;
-//     this.totalMiles = totalMiles;
-//     this.totalFuelValue = totalFuelValue;
-// }
-
-// Car.prototype.drive = function () {
-//     this.totalMiles++;
-//     this.totalFuelValue--;
-// };
-
-// Car.prototype.refuel = function () {
-//     this.totalFuelValue--;
-// }
-
-// const myCar = new Car({
-//     name: 'BMW',
-//     model: 'M10'
-// });
-
-// // myCar.drive();
-// console.log('BMW M10', myCar);
-
-// const herCar = new Car({
-//     name: 'Daewoo',
-//     model: 'Matiz',
-//     totalMiles: 9999999
-// });
-// console.log('Daewoo Matiz', herCar);
-
-
-
-
-
-
-
-
-
-// // ES6
-// class Traktor {
-//     constructor(name, key) {
-//         this.name = name;
-//         this.__myVal = 0;
-//         this[key] = () => {
-//             console.log('myMethod');
-//         }
-//     }
-
-//     get myVal() {
-//         return this.__myVal;
-//     }
-
-//     set myVal(newValue) {
-//         if (typeof newValue !== 'number') return console.error('Ууууу Error');
-
-//         this.__myVal = newValue;
-//     }
-
-
-
-//     singSong() {
-//         // Traktor.getDate();
-//         console.log('MuMuMuMu....');
-//     }
-
-//     static getDate() {
-//         return new Date();
-//     }
-// }
-
-// const myTraktor = new Traktor('Bob', 'method');
-
-// console.log(Traktor.getDate());
-
-// AJAX
-// http://easycode.school/courses?limit=10
-// GET, POST, PUT, DELETE
-// 200, 300, 400, 500 - ответы от сервера
-
-const url = 'https://jsonplaceholder.typicode.com';
-
-// get TODOS
-// const xhr = new XMLHttpRequest();
-
-// xhr.open('GET', `${url}/todos`);
-// xhr.send();
-
-// xhr.addEventListener('load', () => {
-//     console.log(xhr.responseText);
-// });
-
-
-// // get USERS
-// const xhr2 = new XMLHttpRequest();
-
-// xhr2.open('GET', `${url}/users`);
-// xhr2.send();
-
-// xhr2.addEventListener('load', () => {
-//     console.log(xhr2.responseText);
-// });
-
-// TODO: write the CustomHttp class
-class CustomHttp {
-    get(url, callback) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.send();
-        xhr.addEventListener('load', () => {
-            const resp = JSON.parse(xhr.responseText)
-            callback(resp);
-        });
-    }
-    post(url, data, callback) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', url);
-        xhr.setRequestHeader('Content-type', 'application/json')
-        xhr.send(data);
-        xhr.addEventListener('load', () => {
-            const resp = JSON.parse(xhr.responseText)
-            callback(resp);
-        });
-    }
+    loaderUI.setLoader()
+    newsService.getNewsByCountryAndCatigory(({
+        articles
+    }) => {
+        loaderUI.removeLoader()
+        newsUI.addNewsToView(articles)
+    }, country, category)
 }
 
+function onSearchChange() {
+    const search = searchInput.value;
 
+    if (search.length <= 3) return notificationUI.setNotification('Write in the search for more than 3 letters')
 
-const http = new CustomHttp();
-
-// http.get('https://jsonplaceholder.typicode.com/todos', (response) => {
-//     console.log('TODOS --- ', response);
-// });
-
-// http.get('https://jsonplaceholder.typicode.com/users', (response) => {
-//     console.log('USERS ---', response);
-// });
-
-const data = {
-    body: 'Some body',
-    userId: 1,
-    title: 'Some title'
-};
-http.post('https://jsonplaceholder.typicode.com/todos', JSON.stringify(data), (response) => {
-    console.log(response);
-    http.get('https://jsonplaceholder.typicode.com/todos', (response) => {
-        console.log('TODOS --- ', response);
-    });
-});
+    loaderUI.setLoader()
+    newsService.getNewsBySearch(({
+        articles
+    }) => {
+        loaderUI.removeLoader()
+        newsUI.addNewsToView(articles)
+    }, search)
+}
